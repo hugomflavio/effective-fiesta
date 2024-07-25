851f30ec94fbab44dabdf1cd8993e06fa7805fe2
hugomflavio <hmflavio92@gmail.com>
2024-07-25 14:54:33 -0400

    update mean_table

--- Random_R_Functions/mean_table.R
+++ Random_R_Functions/mean_table.R
@@ -1,25 +1,28 @@
-mean_table <- function(x, by, ci = c("none", "sd", "sem"), na.rm = FALSE) {
+mean_table <- function(x, by, ci = c("none", "sd", "sem"),
+					   na.rm = FALSE, digits = Inf) {
 	sem <- function(x, na.rm) {sd(x, na.rm = na.rm) / sqrt(length(x))}
-	
 	ci <- match.arg(ci)
-	output <- as.data.frame(aggregate(x, by, mean, na.rm = na.rm))
-	colnames(output) <- c("group", "mean")
 
-	output$n <- as.vector(aggregate(x, by, function(x) sum(!is.na(x)))$x)
+	output <- as.data.frame(aggregate(x, by, function(x) sum(!is.na(x))))
+	colnames(output)[ncol(output)] <- "n"
 
-	output <- output[, c("group", "n", "mean")]
+	output$mean <- as.vector(aggregate(x, by, mean, na.rm = na.rm)$x)
 
 	if (ci == "sd") {
 		output$sd <- as.vector(aggregate(x, by, sd, na.rm = na.rm)$x)
-		output$ci_up <- output$mean + output$sd
-		output$ci_dn <- output$mean - output$sd
+		output$upper <- round(output$mean + output$sd, digits)
+		output$lower <- round(output$mean - output$sd, digits)
+		output$sd <- round(output$sd, digits)
 	}
 
 	if (ci == "sem") {
 		output$sem <- as.vector(aggregate(x, by, sem, na.rm = na.rm)$x)
-		output$ci_up <- output$mean + output$sem
-		output$ci_dn <- output$mean - output$sem
+		output$upper <- round(utput$mean + output$sem, digits)
+		output$lower <- round(utput$mean - output$sem, digits)
+		output$sem <- round(output$sem, digits)
 	}
 
+	output$mean <- round(output$mean, digits)
+	
 	return(output)
 }
