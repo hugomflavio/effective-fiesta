8e4ac78ea83b0201bd7e8eea86e0e42376672ed4
hugomflavio <hmflavio92@gmail.com>
2024-08-28 21:05:42 -0400

    add patchwork checks to plot_vars (closes #1)

--- Random_R_Functions/plot_vars.R
+++ Random_R_Functions/plot_vars.R
@@ -1,4 +1,11 @@
 plot_vars <- function(input) {
+  if (!nzchar(system.file(package = "patchwork"))) {
+    stop("This function requires the package patchwork.", call. = FALSE)
+  }
+  if (packageVersion("patchwork") < "1.2.0") {
+    stop("This function requires version 1.2.0 or higher of package patchwork.",
+         call. = FALSE)
+  }
   if (is.numeric(input[, 1])) {
     j_height <- 0
   } else {
@@ -11,9 +18,11 @@ plot_vars <- function(input) {
     } else {
       j_width <- 0.2
     }
-    p <- ggplot(data = input, aes(y = .data[[colnames(input)[1]]], x = .data[[colnames(input)[i]]]))
-    p <- p + geom_point(
-      position = position_jitter(
+    p <- ggplot2::ggplot(data = input, 
+      ggplot2::aes(y = .data[[colnames(input)[1]]], 
+                   x = .data[[colnames(input)[i]]]))
+    p <- p + ggplot2::geom_point(
+      position = ggplot2::position_jitter(
         width = j_width, height = j_height), 
       alpha = 0.2)
     p
